9e3350ecd50001b49fd646c1e032f43b00f84ba9
unes0001@algonquinlive.com <unes0001@algonquinlive.com>
2026-01-17 00:39:52 -0500

    Refactor code structure and remove redundant sections for improved readability and maintainability

--- JS/script.js
+++ JS/script.js
@@ -67,3 +67,14 @@ function scrollToTop() {
     });
 }
 
+
+
+//hamburger menu
+
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.nav-links');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
